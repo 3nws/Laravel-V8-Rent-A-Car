2c205687113696e49637a6d2ff340924233944a4
Enes <eneskurbetoglu1@gmail.com>
2021-11-28 15:35:05 +0300

    extending layouts, learning sections and yields to use later

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,12 +3,19 @@
 @section('title', 'index page')
 
 @section('sidebar')
-    @parent
-
-    <p>This is appended to the master sidebar.</p>
+    <div class="col-sm-3 col-lg-2">
+        <ul>
+            <li>link 1</li>
+            <li>link 2</li>
+            <li>link 3</li>
+            <li>link 4</li>
+        </ul>
+    </div>
 @endsection
 
 
 @section('content')
-    <p>This is the body content!</p>
+    <div class="col-sm-9 col-lg-10">
+        <p>This is the body content!</p>
+    </div>
 @endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,16 +5,31 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>App Name - @yield('title')</title>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    @yield('css')
+    @yield('js')
 </head>
 <body>
 
-    @section('sidebar')
-        This is the master sidebar.
-    @show
+    <div class="jumbotron text-center">
+        <h1>Header</h1>
+        <p>Resize this responsive page to see the effect</p>
+        @yield('header')
+    </div>
 
     <div class="container">
-        @yield('content')
+        <div class="row">
+            @yield('sidebar')
+            @yield('content')
+        </div>
+    </div>
+
+    <div class="jumbotron text-center">
+        <h1>Footer</h1>
+        <p>footer</p>
+        @yield('footer')
     </div>
 
 </body>
