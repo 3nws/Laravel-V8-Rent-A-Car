46c5c24f4d0c264a6df75955e1fa2016550fd3e5
Enes <eneskurbetoglu1@gmail.com>
2021-12-14 10:09:13 +0300

    admin controller and template set up

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,24 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/home2', function (){
+    return view('welcome');
+});
+
+Route::redirect('/homepage', 'home')->name('homepage');
+
+Route::get('/', function (){
+    return view('home.index');
+});
+
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/aboutus', [HomeController::class, 'aboutus'])->name('aboutus');
+
+
+// Admin routes
+Route::get('/admin', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('adminhome');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
