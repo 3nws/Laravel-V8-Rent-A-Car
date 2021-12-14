465e0b53f41da915ab727f18c4243ec2e48a5ff6
Enes <eneskurbetoglu1@gmail.com>
2021-12-14 11:06:02 +0300

    admin login actions

--- resources/views/admin/_content.blade.php
+++ resources/views/admin/_content.blade.php
@@ -173,10 +173,11 @@
                 <div class="topbar-divider d-none d-sm-block"></div>
 
                 <!-- Nav Item - User Information -->
+                @auth
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
                         <img class="img-profile rounded-circle"
                              src="{{ asset('assets') }}/img/undraw_profile.svg">
                     </a>
@@ -196,13 +197,13 @@
                             Activity Log
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                        <a class="dropdown-item" href="{{ route('admin_logout') }}" data-toggle="modal" data-target="#logoutModal">
                             <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                             Logout
                         </a>
                     </div>
                 </li>
-
+                @endauth
             </ul>
 
         </nav>
