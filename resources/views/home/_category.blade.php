61b7458dbaf1526115f7006cf42d1f5d9e162b9c
Enes <eneskurbetoglu1@gmail.com>
2021-12-30 11:34:13 +0300

    failing miserably in creating a multi level category dropdown menu

--- resources/views/home/_category.blade.php
+++ resources/views/home/_category.blade.php
@@ -3,20 +3,62 @@
 @endphp
 
 
-<div class="dropdown no-arrow mb-4 nav-link">
-    <a class="" type="button" style="color: white;"
-       id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-       aria-expanded="false">
-        Categories
-    </a>
-    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+<li class="nav-item dropdown">
+    <a id="dropdownMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Categories</a>
+    <ul aria-labelledby="dropdownMenu1" class="dropdown-menu border-0 shadow">
         @foreach($parentCategories as $rs)
-            <a class="dropdown-item" href="{{ route('category_cars', ['id' => $rs->id]) }}">{{ $rs->title }}</a>
+            @if(count($rs->children))
+                <li class="dropdown-submenu">
+                    <a href="#" class="dropdown-item">{{ $rs->title }}</a>
+                    @include('home.categoryTree', ['children' => $rs->children])
+                </li>
+            @else
+                <li><a href="#" class="dropdown-item">{{ $rs->title }}</a></li>
+            @endif
+        @endforeach
+    </ul>
+</li>
+
+
+{{--<div class="dropdown no-arrow mb-4 nav-link">--}}
+{{--    <a class="" type="button" style="color: white;"--}}
+{{--       id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"--}}
+{{--       aria-expanded="false">--}}
+{{--        Categories--}}
+{{--    </a>--}}
+{{--    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">--}}
+{{--        @foreach($parentCategories as $rs)--}}
+{{--            <a class="dropdown-item" href="{{ route('category_cars', ['id' => $rs->id]) }}">{{ $rs->title }}</a>--}}
 {{--            <div class="dropdown-menu">--}}
 {{--                @if(count($rs->children))--}}
 {{--                    @include('home.categoryTree', ['children' => $rs->children])--}}
 {{--                @endif--}}
 {{--            </div>--}}
-        @endforeach
-    </div>
-</div>
+{{--        @endforeach--}}
+{{--    </div>--}}
+{{--</div>--}}
+
+@section('footerjs')
+    <script>
+        $(function() {
+            // ------------------------------------------------------- //
+            // Multi Level dropdowns
+            // ------------------------------------------------------ //
+            $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+
+                $(this).siblings().toggleClass("show");
+
+
+                if (!$(this).next().hasClass('show')) {
+                    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+                }
+                $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+                    $('.dropdown-submenu .show').removeClass("show");
+                });
+
+            });
+        });
+    </script>
+@endsection
--- resources/views/home/categoryTree.blade.php
+++ resources/views/home/categoryTree.blade.php
@@ -1,4 +1,24 @@
 {{--@foreach($children as $subcategory)--}}
+
+{{--    @if(count($subcategory->children))--}}
+{{--        @include('home.categoryTree', ['children' => $rs->children])--}}
+{{--    @else--}}
+{{--      <a id="dropdownMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Hover for action</a>--}}
+{{--      <ul aria-labelledby="dropdownMenu2" class="dropdown-menu border-0 shadow">--}}
+{{--          <li>--}}
+{{--              <a href="#" class="dropdown-item">{{ $subcategory->title }}</a>--}}
+{{--          </li>--}}
+{{--          @foreach($parentCategories as $rs)--}}
+{{--              <a class="dropdown-item" href="#">o{{ $rs->title }}</a>--}}
+{{--          @endforeach--}}
+{{--      </ul>--}}
+{{--    @endif--}}
+
+{{--@endforeach--}}
+
+
+
+{{--@foreach($children as $subcategory)--}}
 {{--    <div class="dropdown no-arrow mb-4 nav-link">--}}
 {{--        @if(count($subcategory->children))--}}
 {{--            @include('home.categoryTree', ['children' => $rs->children])--}}
